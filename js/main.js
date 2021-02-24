f0c2e3ea3f2b60cea086821416f46ffa061c6c41
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2021-02-24 18:03:27 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,6 @@ var config = {
     };
 
 var game = new Phaser.Game(config);
-var name = String(decodeURIComponent(window.location.search)).slice(6); //Имя игрока
+var name = String(decodeURIComponent(window.location.search)).slice(6, String(decodeURIComponent(window.location.search)).indexOf('&')-6); //Имя игрока
 var total_score = 0;
 
